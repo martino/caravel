1c8f66cdd7cb7db54eae7a214bd6e23b096451ce
Martino Pizzol <martino.pizzol@gmail.com>
2016-06-03 10:15:53 +0200

    Update timing for multiple worker

--- caravel/models.py
+++ caravel/models.py
@@ -726,16 +726,14 @@ class SqlaTable(Model, Queryable, AuditMixinNullable):
             qry.compile(
                 engine, compile_kwargs={"literal_binds": True},),
             )
+        
         start_date = datetime.now()
-        print(sql)
-
         df = pd.read_sql_query(
             sql=sql,
             con=engine
         )
-
         time_elapsed = datetime.now() - start_date
-        print("executed in {} seconds".format(time_elapsed.total_seconds()))
+        print("{}\nExecuted in {} seconds\n".format(sql, time_elapsed.total_seconds()))
 
         sql = sqlparse.format(sql, reindent=True)
         return QueryResult(
